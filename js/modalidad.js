2e0e4d6fd47c44d2cbba7e8dbda689a0db1689e2
Pablo Botta <pabloluisbotta@gmail.com>
2018-03-28 16:05:35 -0300

    Modalidad transporte

---
+++ js/modalidad.js
@@ -0,0 +1,12 @@
+new Vue({
+    el: '#app',
+    data: {
+        name: 'ciudad',
+        transport: {
+            bus: 25,
+            bike: 25,
+            car: 25,
+            walk: 25
+        }
+    }
+})
